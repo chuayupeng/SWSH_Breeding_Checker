d5515a7d225ee97fece04ab49339b0d63f03fbb8
chuayupeng <chua.yupeng@dhs.sg>
2019-11-23 01:20:22 +0800

    fixed bfs, updated for qol user interface

--- swsh_breeding_checker.py
+++ swsh_breeding_checker.py
@@ -1,3 +1,5 @@
+import sys
+
 class Pokemon:
     """
     Simple Node class
@@ -15,6 +17,9 @@ class Pokemon:
         self.distance = -1              # shortest distance from source node in shortest path search
         self.previous = None            # previous vertex in shortest path search
 
+    def getName(self):
+        return self.pokemon
+
     def addNeighbour(self, pokemon):
         """
         Adds a new vertex as an adjacent neighbor of this vertex
@@ -30,13 +35,15 @@ class Pokemon:
         """
         return self.eggGroups
     
-    def getNeighbors(self):
+    def getNeighbours(self):
         """
         Returns a list of all neighboring pokemon
         :return: list of Pokemon
         """
         return self.neighbors
 
+    def getPrev(self):
+        return self.previous
 
 def shortestPathBFS(pokemon):
     """
@@ -45,7 +52,8 @@ def shortestPathBFS(pokemon):
     :return: does not return, changes in place
     """
     if pokemon is None:
-        return
+        print("(-) Invalid source pokemon, did you check the sword and shield dex cuts?")
+        sys.exit(-1)
 
     queue = []                                  # our queue is a list with insert(0) as enqueue() and pop() as dequeue()
     queue.insert(0, pokemon)
@@ -53,13 +61,15 @@ def shortestPathBFS(pokemon):
     while len(queue) > 0:
         current_pokemon = queue.pop()                    # remove the next node in the queue
         next_distance = current_pokemon.distance + 1     # the hypothetical distance of the neighboring node
-
-        for neighbor in current_pokemon.getNeighbors():
-            if neighbor.distance == -1 or neighbor.distance > next_distance:    # try to minimize node distance
-                neighbor.distance = next_distance       # distance is changed only if its shorter than the current
-                neighbor.previous = current_pokemon      # keep a record of previous vertexes so we can traverse our path
-                queue.insert(0, neighbor)
-
+        if next_distance == 0:
+            current_pokemon.distance = -2
+        for neighbour in current_pokemon.getNeighbours():
+            if neighbour.distance == -1 or neighbour.distance > next_distance:    # try to minimize node distance
+                neighbour.distance = next_distance       # distance is changed only if its shorter than the current
+                neighbour.previous = current_pokemon      # keep a record of previous vertexes so we can traverse our path
+                queue.insert(0, neighbour)
+            
+    
 
 def traverseShortestPath(targetmon):
     """
@@ -69,10 +79,14 @@ def traverseShortestPath(targetmon):
     """
     vertexes_in_path = []
 
-    while targetmon.previous:
+    if targetmon is None:
+        print("(-) Invalid target pokemon! Did you check the dex cuts?")
+        sys.exit(-1)
+
+    while targetmon.previous != None:
         vertexes_in_path.append(targetmon.pokemon)
         targetmon = targetmon.previous
-
+    vertexes_in_path.append(targetmon.pokemon)
     return vertexes_in_path
 
 
@@ -81,20 +95,31 @@ def populateBFS(monDict, typesDict):
     for mon in monDict:
         monTypes = monDict[mon]
         PokemonNodeDict[mon] = Pokemon(mon, monTypes)
-
     for mon in PokemonNodeDict:
         node = PokemonNodeDict[mon]
         for eggGroup in node.getEggGroups():
             for neighbour in typesDict[eggGroup]:
                 if neighbour == mon:
                     continue
-                else:
+                elif PokemonNodeDict[neighbour] not in node.getNeighbours():
                     node.addNeighbour(PokemonNodeDict[neighbour])
-            
+                else:
+                    continue
+######## DEBUG
+#        print()
+#        print(node.getName())
+#        print("Neighbours:")
+#        for neighbour in node.getNeighbours():
+#            print(neighbour.getName())
+
     return PokemonNodeDict
 
 def main():
-    #clean data
+    if len(sys.argv) != 3:
+        print("(+) usage: python3 %s source_pokemon target_pokemon" % sys.argv[0])
+        print("(+) eg. python3 %s eevee charmander" % sys.argv[0])
+        sys.exit(-1)
+    # clean data
     file = open("egggroups.txt", "r")
     lines = file.readlines()
     monDict = {}
@@ -110,11 +135,20 @@ def main():
                 typesDict[eggType] = [name]
             else:
                 typesDict[eggType].append(name)
-                
+#### DEBUG
+#    print(typesDict)
+#    print(monDict)
     nodeDict = populateBFS(monDict, typesDict)
-    source = "Eevee"
-    target = "Charmander"
-    shortestPathBFS(nodeDict[source])
-    pokemon_to_breed = traverseShortestPath(nodeDict[target])
-            
-main()
+    source = sys.argv[1].capitalize()
+    target = sys.argv[2].capitalize()
+    if source in nodeDict.keys() and target in nodeDict.keys():
+        shortestPathBFS(nodeDict[source])
+        pokemon_to_breed = traverseShortestPath(nodeDict[target])
+        seperator = " -> "
+        print(seperator.join(list(reversed(pokemon_to_breed))))
+    else:
+        print("(-) Invalid source or target pokemon! Did you check the dex cuts? Also pst Ditto or Legendaries aren't included for obvious reasons.")
+        sys.exit(-1)
+
+if __name__ == "__main__":
+    main()
